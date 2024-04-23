5f81a2bfb617f7eeefc3cff2579d5ef1635c7064
thenav56 <ayernavin@gmail.com>
2024-04-23 22:45:37 +0545

    Move collectstatic and migrate to worker

--- deploy/run_web.sh
+++ deploy/run_web.sh
@@ -6,9 +6,4 @@ ROOT_DIR=$(dirname "$BASE_DIR")
 
 cd $ROOT_DIR
 
-echo '>> [Running] Django Collectstatic and Migrate'
-
-./manage.py collectstatic --no-input
-./manage.py migrate --no-input
-
 uwsgi --ini ./deploy/uwsgi.ini # Start uwsgi server
--- deploy/run_worker.sh
+++ deploy/run_worker.sh
@@ -6,5 +6,10 @@ ROOT_DIR=$(dirname "$BASE_DIR")
 
 cd $ROOT_DIR
 
+echo '>> [Running] Django Collectstatic and Migrate'
+
+./manage.py collectstatic --no-input
+./manage.py migrate --no-input
+
 # Start celery
 celery -A main worker -Q CELERY-DEFAULT-QUEUE,CELERY-EXPORT-HEAVY-QUEUE -E --concurrency=2 -l info
